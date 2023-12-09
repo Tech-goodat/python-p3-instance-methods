a36b2a337f9c1f970fdd5278dc220759554210b4
Tech-goodat <felixkiprotich2000@gmail.com>
2023-12-09 16:00:12 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,12 +1,11 @@
 class Dog:
     def bark(self):
         print("Woof!")
+
     def sit(self):
         print("The dog is sitting.")
 fido=Dog()
 fido.bark()
-fido.sit()
 
-snoopy=Dog()
-snoopy.bark()
-        
+snoop_dog=Dog()
+snoop_dog.sit()
